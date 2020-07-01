5b50a514bbf9af16a9f022d1bf46867178d98fb5
Ruel Valencia <ruelvalencia@gmail.com>
2020-07-01 09:28:18 +0800

    Added eth & mcu reset js files

---
+++ rubix-compute/pi-compute/scripts/eth_reset.js
@@ -0,0 +1,8 @@
+const Gpio = require('pigpio').Gpio;
+
+const mcu_reset = new Gpio(26, {mode: Gpio.OUTPUT});
+
+mcu_reset.digitalWrite(0);
+
+setTimeout(function(){ mcu_reset.digitalWrite(1);}, 2000);
+
